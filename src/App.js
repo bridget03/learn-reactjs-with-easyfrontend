78234fc5c45c910f20332a8b78f0152a51f51923
Phillip Lam <nguyenlam2904@gmail.com>
2024-02-26 21:38:08 +0700

    update

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React With Easy frontend
         </a>
       </header>
     </div>
