8353ee050cd003a11f8a1c06eb29ebb50b10e337
Phillip Lam <nguyenlam2904@gmail.com>
2024-02-27 21:17:46 +0700

    update21:17

--- src/App.js
+++ src/App.js
@@ -2,12 +2,25 @@ import ColorBox from "./ColorBox";
 import React from "react";
 
 export default function App() {
+    const name = "Lam";
+    const age = 20;
+    const isMale = true;
+    const student ={
+        name: "Phil Nguyen"
+    };
+    const colorList = ['red', 'green', 'blue'];
     return (
-        <section>
-            <ColorBox color="red"></ColorBox>
-            <ColorBox color="green"></ColorBox>
-            <ColorBox color="blue"></ColorBox>
-        </section>
+        <>
+            <section>
+                <ColorBox color="red"></ColorBox>
+                <ColorBox color="green"></ColorBox>
+                <ColorBox color="blue"></ColorBox>
+            </section>
+            <p>Hello, I'm {name}, I'm {age} - {isMale ? 'Male' : 'Female'}</p>
+            {isMale ? <p>Male</p> : <p>Female</p>}
+            {isMale && <p>Male</p>}
+            {!isMale && <p>Female</p>}
+        </>
     );
 
 }
