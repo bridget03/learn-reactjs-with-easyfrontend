7f420cbc21f34b7a6caa9a60e698e61389f9c062
phil lam <nguyenlam2904@gmail.com>
2024-03-03 15:24:21 +0700

    mac1

--- src/components/features/Todo/index.jsx
+++ src/components/features/Todo/index.jsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import DetailPage from './pages/DetailPage';
-import ListPage from './pages/ListPage';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import DetailPage from "./pages/DetailPage";
+import ListPage from "./pages/ListPage";
 
-TodoFeature.propTypes = {
-    
-};
+TodoFeature.propTypes = {};
 
 function TodoFeature(props) {
-    // const match = matchPath();
+  // const match = matchPath();
 
-    return (
-        <div>
-            Todo Feature
-            <Routes>
-                <Route path='/todo/listpage' element={<ListPage/>}/>
-                <Route path='todo/:todoId' element={<DetailPage/>}/>
-            </Routes>
-        </div>
-    );
+  return (
+    <div>
+      Todo Featurekgiuh
+      <Routes>
+        <Route path="/todo/listpage" element={<ListPage />} />
+        <Route path="todo/:todoId" element={<DetailPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default TodoFeature;
